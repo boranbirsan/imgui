c90b915971a6f4becae2788c2a1900b5daccd8d7
Boran-Birsan <boran.birsan@gmail.com>
2021-03-01 18:08:02 +0300

    updated premake

--- premake5.lua
+++ premake5.lua
@@ -13,6 +13,7 @@ project "ImGui"
 		"imgui_draw.cpp",
 		"imgui_internal.h",
 		"imgui_widgets.cpp",
+        "imgui_tables.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
@@ -32,8 +33,8 @@ project "ImGui"
 
 	filter "configurations:Debug"
 		runtime "Debug"
-		symbols "on"
+		symbols "On"
 
 	filter "configurations:Release"
 		runtime "Release"
-		optimize "on"
+		optimize "On"
